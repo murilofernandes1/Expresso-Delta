91db770783c48a52d9a0d2ea3e333af12fa3a8a9
Murilo <murigod8@gmail.com>
2025-07-23 19:55:39 -0300

    changed the router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./css/index.css";
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { Route, Routes, HashRouter } from "react-router-dom";
 import App from "./App";
 import Coleta from "./routes/Coleta";
 import Sobre from "./routes/Sobre";
@@ -10,12 +10,12 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/Coleta" element={<Coleta />} />
         <Route path="/Sobre" element={<Sobre />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
